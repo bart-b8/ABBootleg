c8f0156eaa5f7c758301d158e4f7d9360f7315d6
Bart Decoutere <bart.decoutere@ugent.be>
2023-05-16 18:55:01 +0200

    fill out header file Menu.h

--- Menu.h
+++ Menu.h
@@ -3,6 +3,32 @@
 
 #include "Allkit.h"
 
+enum menu {
+  START_MENU,
+  LEVEL_MENU,
+  SCORE_MENU
+};
+
+enum menu_actions {
+  START_GAME,
+  OPEN_PREV,
+  OPEN_REPLAY,
+  OPEN_LEVELMENU,
+  OPEN_REPLAY,
+  BACK
+
+}
+
+class Menu_item {
+  public:
+    Menu_item(std::string text, enum menu_actions): text(text), menu_actions(menu_actions);
+  private:
+    std::string text;
+    enum menu_actions;
+    bool action(enum menu_actions);
+
+}
+
 class Menu {
   public:
     Menu() : ak_(&Allkit::Get()){};
@@ -15,8 +41,34 @@ class Menu {
   private:
     Allkit *ak_ = NULL;
 
+    int selected = 0;
+
+    std::vector<Menu_item> menu_items = select_menu(START_MENU);
+
+    std::vector<Menu_item> select_menu(enum menu);
+
+    void display_menu(int index);
+
+    void display_menu(enum menu, int index=0);
+
   protected:
     bool exit_ = false;
 };
+
+/* Create Menu's */
+std::vector<Menu_item> start_menu, level_menu, score_menu;
+
+/*Construct start_menu*/
+start_menu.push_back(Menu_item("start", OPEN_LEVELMENU));
+start_menu.push_back(Menu_item("replay", OPEN_REPLAYMENU));
+start_menu.push_back(Menu_item("quit", QUIT));
+
+/*Construct level_menu*/
+level_menu.push_back(Menu_item("quit", BACK));
+
+/*Construct score_menu*/
+score_menu.push_back(Menu_item("quit", BACK))
+
+
 
 #endif
