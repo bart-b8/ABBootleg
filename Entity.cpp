e0e9f03ac683652e6e54edec115244300295dc9e
munazir <muhammad.nazir@ugent.be>
2023-05-18 21:45:31 +0200

    completed Entity header and implementation files

--- Entity.cpp
+++ Entity.cpp
@@ -1,23 +1,37 @@
 #include "Entity.h"
 
-/*
+
+
 void Entity::Add(Component* component) {
-    // TODO
+    Comps.push_back(component);
 }
 
 void Entity::Remove(Component* component) {
-    // TODO
+    auto re=std::find(Comps.begin(),Comps.end(),component);
+    if (re!=Comps.end()) {
+        Comps.erase(re);
+    }
 }
 
 Component* Entity::GetComponent(Component::Tag tag) {
-    // TODO
+    for (auto component:Comps) { // for loop die alleen werkt met begin en end dingen
+        if (component->GetTag()==tag){
+            return component;
+        }
+
+    }
+    return nullptr; //component niet gevonden in vector met opgegeven tag
 }
 
 std::vector<Component::Tag>& Entity::GetTags() {
-    // TODO
+    vec_tags.clear();
+    for (auto component:Comps) {
+        vec_tags.push_back(component->GetTag());
+    }
+    return vec_tags;
 }
 
 std::vector<Component*>& Entity::GetComponents() {
-    // TODO
+    return Comps;
 }
-*/
+
