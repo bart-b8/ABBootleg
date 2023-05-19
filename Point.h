2aa1284f75f0e6c7838aa7ee3b4a57861d4db96e
Bart Decoutere <bart.decoutere@ugent.be>
2023-05-19 17:50:28 +0200

    Compleets point.h

--- Point.h
+++ Point.h
@@ -10,27 +10,28 @@ class Point {
     Point(){};
     Point(double x, double y) : x_(x), y_(y){};
 
-    double operator*(const Point &o) {
-        // TODO
-        return 0;
-    }
-
     Point operator-(const Point &o) {
         return Point(x_-o.x_,y_-o.y_);
     }
 
     double operator>>(const Point &o) {
-        // TODO
-        return 0;
+        return x_*o.x_ + y_*o.y_ ;
     }
 
     double Length() {
-        // TODO
         return sqrt(pow(x_,2) + pow(y_,2));
     }
 
+    double operator*(const Point &o) {
+        return (*this-o).Length();
+    }
+
     void Normalize() {
-        // TODO
+        double l = Length();
+        if (1!=0.0) {
+            x_ = x_/l;
+            y_ = y_/l; 
+        }
     }
 };
 
