045c008dae3bd70d7304650f7f029d74a5ba824b
munazir <muhammad.nazir@ugent.be>
2023-05-21 14:32:14 +0200

    launcher and missile system headers edited

---
+++ LauncherSystem.h
@@ -0,0 +1,17 @@
+#ifndef LAUNCHERSYSTEM_H
+#define LAUNCHERSYSTEM_H
+
+#include <vector>
+
+#include "Point.h"
+#include "System.h"
+
+class LauncherSystem : public System {
+  public:
+    void Update();
+
+  private:
+
+};
+
+#endif
---
+++ MissileSystem.h
@@ -0,0 +1,17 @@
+#ifndef MISSILESYSTEM_H
+#define MISSILESYSTEM_H
+
+#include <vector>
+
+#include "Point.h"
+#include "System.h"
+
+class MissileSystem : public System {
+  public:
+    void Update();
+
+  private:
+
+};
+
+#endif
