9cd1e22978847683795e93e7cfaa0e7455f9b852
Bart Decoutere <bart.decoutere@ugent.be>
2023-08-14 18:26:40 +0200

    correct style Point.h

--- Point.h
+++ Point.h
@@ -5,37 +5,27 @@
 #include <math.h>
 
 class Point {
-  public:
-    double x_, y_;
+public:
+  double x_, y_;
 
-    Point(){};
-    Point(double x, double y) : x_(x), y_(y){};
+  Point() {}
+  Point(double x, double y) : x_(x), y_(y) {}
 
+  Point operator-(const Point &o) { return Point(x_ - o.x_, y_ - o.y_); }
 
-    Point operator-(const Point &o) {
-        return Point(x_-o.x_,y_-o.y_);
+  double operator>>(const Point &o) { return x_ * o.x_ + y_ * o.y_; }
+
+  double Length() { return sqrt(pow(x_, 2) + pow(y_, 2)); }
+
+  double operator*(const Point &o) { return (*this - o).Length(); }
+
+  void Normalize() {
+    double l = Length();
+    if (1 != 0.0) {
+      x_ = x_ / l;
+      y_ = y_ / l;
     }
-
-    double operator>>(const Point &o) {
-        return x_*o.x_ + y_*o.y_ ;
-    }
-
-    double Length() {
-        return sqrt(pow(x_,2) + pow(y_,2));
-    }
-
-    double operator*(const Point &o) {
-        return (*this-o).Length();
-    }
-
-    void Normalize() {
-        double l = Length();
-        if (1!=0.0) {
-            x_ = x_/l;
-            y_ = y_/l; 
-
-        }
-    }
+  }
 };
 
 #endif
